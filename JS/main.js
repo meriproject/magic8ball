8b0db5d4745dd1448aba87fc7b2ddc52261fd399
meriproject <merischool1621@gmail.com>
2022-02-15 17:37:33 -0800

    first phase

---
+++ JS/main.js
@@ -0,0 +1,10 @@
+
+const imgball=document.getElementById('ballimg');
+const btnask=document.getElementById('question');
+let answer = document.getElementById('answer')
+btnask.addEventListener('click', () => {
+  let question= prompt("Ask me what you want to know");
+  imgball.effect("shaking");
+
+});
+
